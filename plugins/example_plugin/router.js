5f64439f573473469a79180ed1b23f5d6b492fa9
Hydren <166227780+hydren-dev@users.noreply.github.com>
2024-12-27 12:40:09 +0500

    Update router.js

--- plugins/example_plugin/router.js
+++ plugins/example_plugin/router.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const { db } = require('../../handlers/db')
-# you need to add
-# "instancesidebar": {
-#       "example url": {
-#           "url": "/instance/%id%/example",
-#           "icon": "fa-solid fa-folder"
-#       }
-#   }
+const { db } = require('../../handlers/db');
+
+// Add the following to your manifest.json file to define the instance sidebar:
+// "instancesidebar": {
+//     "example url": {
+//         "url": "/instance/%id%/example",
+//         "icon": "fa-solid fa-folder"
+//     }
+// }
 
 router.get('/instance/:id/example', (req, res) => {
     const { id } = req.params;
 
     res.render('index', {
-        user: req.user,
-        req
+        user: req.user, // Pass the user object to the view
+        req,            // Pass the request object for additional context
     });
 });
 
