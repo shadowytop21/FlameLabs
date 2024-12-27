188ace0c90c590e2412da4a9a08ec91e87cda1e3
Hydren <166227780+hydren-dev@users.noreply.github.com>
2024-12-27 12:39:21 +0500

    Update router.js

--- plugins/example_plugin/router.js
+++ plugins/example_plugin/router.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { db } = require('../../handlers/db')
+# you need to add
+# "instancesidebar": {
+#       "example url": {
+#           "url": "/instance/%id%/example",
+#           "icon": "fa-solid fa-folder"
+#       }
+#   }
 
 router.get('/instance/:id/example', (req, res) => {
     const { id } = req.params;
